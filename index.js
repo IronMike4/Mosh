f9d9979705b07dea9780e75914b97823c2bceaa9
Michael Mahachi <Michael.Mahachi@derivco.co.za>
2022-05-18 19:58:30 +0200

    objects

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const interestRate = 0.3;
-interestRate = 1;
-console.log(interestRate);
+let person = { 
+    name: 'Mike',
+    age: 30
+};
+
+// Dot Notation
+person.name = 'John';
+
+// Bracket Notation
+person['name'] = 'Mary';
+console.log(person.name);
